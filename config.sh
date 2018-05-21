4ce4075fd148ac122deee3339590ee670f37479f
Brandon Sanders <brandon@alicorn.io>
2018-05-21 18:50:17 -0400

    Update configuration script to be very portable.

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 # Change this value to match the IP address that your clients will use
 # when connecting to the Darkstar server.
-export ZONE_IP="adharc.ddns.net"
+export ZONE_IP=${ZONE_IP:-$(dig +short myip.opendns.com @resolver1.opendns.com)}
 
 # Username and password to use for the database.
-export DS_USERNAME="darkstar"
-export DS_PASSWORD="dockstar"
+export DS_USERNAME=${DS_USERNAME:-"darkstar"}
+export DS_PASSWORD=${DS_PASSWORD:-"dockstar"}
 
 # Name of the server.
-export DS_SERVERNAME="Dockstar"
+export DS_SERVERNAME=${DS_SERVERNAME:-"Dockstar"}
