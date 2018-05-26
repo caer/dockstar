1cc081108c00dea5390ad59f46b6fc717873029b
Brandon Sanders <brandon@alicorn.io>
2018-05-26 11:58:33 -0400

    Play with defaults and make the IP detection work for local IPs by default.

--- src/scripts/env.sh
+++ src/scripts/env.sh
@@ -3,8 +3,13 @@
 export DRK_GMS_LIST=${DRK_GMS_LIST:-""}
 
 # Change this value to match the IP address that your clients will use
-# when connecting to the Darkstar server. Defaults to the public IP of the server.
-export DRK_ZONE_IP=${DRK_ZONE_IP:-$(dig +short myip.opendns.com @resolver1.opendns.com)}
+# when connecting to the Darkstar server.
+
+# Uncomment this line to default to the public IP of the server.
+# export DRK_ZONE_IP=${DRK_ZONE_IP:-$(dig +short myip.opendns.com @resolver1.opendns.com)}
+
+# Uncomment this line to default to the local IP of the server.
+export DRK_ZONE_IP=${DRK_ZONE_IP:-$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1')}
 
 # Host of the database. When using the containers, this
 # does not need to be modified.
